857686845ff318f8daaad4f2ff29353b1d351c74
zhenzar <zhenzar@hotmail.com>
2026-04-25 12:49:03 +0200

    Fix missing Auth facade import

--- app/Http/Controllers/MudLogController.php
+++ app/Http/Controllers/MudLogController.php
@@ -10,6 +10,7 @@ use App\Models\Item;
 use App\Models\LogFile;
 use App\Services\LogScanner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class MudLogController extends Controller
